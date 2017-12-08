a5e4dab69e822eaecd06a1f79549ba5bc09fdf69
Luchang Li <34341633+luchangli03@users.noreply.github.com>
2017-12-08 09:54:29 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,14 +29,18 @@ int main()
 
 
 
-	float ix1[DatLen];
-	float iy1[DatLen] = { 1.03787663582584, 1.83038049957306, 2.97498249283382, 4.45630428499173, 6.15194791898853, 7.82704538241868, 9.17762784414991, 9.91768757606545, 9.87728971930251, 9.06593396336589, 7.66892868588658, 5.97866523316379, 4.29557358210739, 2.84436599826735, 1.73579000492169 };
+	int ix1[DatLen];
+	int iy1[DatLen] = { 1, 1, 2, 4, 6, 7, 9, 9, 9, 9, 7, 5, 4, 2, 1 };
 
 	for (cnt = 0; cnt < DatLen; cnt++)
 	{
 		ix1[cnt] = cnt;
 	}
-	BFGSOptimizer< float, 3, 6, 11> GausCurveFit(GausFit10_PreFitting, GausFit10_TargerF);
+	BFGSOptimizer< int, 3, 6, 11> GausCurveFit(NULL, GausFit10i_TargerF);
+	GausCurveFit.FitPara[0] = 9;
+	GausCurveFit.FitPara[1] = 7;
+	GausCurveFit.FitPara[2] = 3;
+
 	GausCurveFit.BFGSOptimize(ix1, iy1, DatLen);
 
 
