24c71d2020277488b0badbeef94027dfdad89b0a
Luchang Li <34341633+luchangli03@users.noreply.github.com>
2017-12-08 08:44:54 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
 #define DatLen   15
 
 	// ParaNum, ItNum, ItNum_bs; 
-	BFGSOptimizer< 2, 4, 11> ExpCurveFit(ExpFit_PreFitting, ExpFit_TargerF);
+	BFGSOptimizer< float, 2, 4, 11> ExpCurveFit(ExpFit_PreFitting, ExpFit_TargerF);
 
 	float ix[DatLen];
 	float iy[DatLen] = { 0.561983237858735, 0.274092050681491, 0.0936873888873212, 0.0357818099726275, 0.0158309111945142, 0.00702655416654909, 0.00437395942094421, 0.00214465107091458, 0.00146739283799419, 0.000649039139882044, 0.000705477325958744, 0.000649039139882044, 0.000225752744306798, 0.000310410023421847, 0.000169314558230098 };
@@ -23,12 +23,11 @@ int main()
 		ix[cnt] = cnt;
 	}
 
-	ExpCurveFit.BFGSOptimizer_Top(ix, iy, DatLen);
+	ExpCurveFit.BFGSOptimize(ix, iy, DatLen);
 	ExpCurveFit.PrintfFitPara("final");
 
 
 
-	BFGSOptimizer< 4, 8, 11> GausCurveFit(GausFit11_PreFitting, GausFit11_TargerF);
 
 	float ix1[DatLen];
 	float iy1[DatLen] = { 1.03787663582584, 1.83038049957306, 2.97498249283382, 4.45630428499173, 6.15194791898853, 7.82704538241868, 9.17762784414991, 9.91768757606545, 9.87728971930251, 9.06593396336589, 7.66892868588658, 5.97866523316379, 4.29557358210739, 2.84436599826735, 1.73579000492169 };
@@ -37,8 +36,9 @@ int main()
 	{
 		ix1[cnt] = cnt;
 	}
+	BFGSOptimizer< float, 3, 6, 11> GausCurveFit(GausFit10_PreFitting, GausFit10_TargerF);
+	GausCurveFit.BFGSOptimize(ix1, iy1, DatLen);
 
-	GausCurveFit.BFGSOptimizer_Top(ix1, iy1, DatLen);
 
 	GausCurveFit.PrintfFitPara("final");
 
